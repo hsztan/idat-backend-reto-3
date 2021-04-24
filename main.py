84c0344bd474d2645a20ce3de5e5f5371492d5e7
Henry Nawrocki <hdev@sztanski.com>
2021-04-24 11:55:34 -0500

    first commit reto 3

---
+++ main.py
@@ -0,0 +1,39 @@
+alumnos = []
+cantidad_alumnos = -1
+
+while cantidad_alumnos == -1:
+    try:
+        cantidad_alumnos = int(
+            input('Ingresar cantidad de alumnos a ingresar: '))
+    except:
+        print("Ingrese solo números")
+
+for count in range(cantidad_alumnos):
+    # crear objeto alumno
+    alumnos.append({})
+    alumno = alumnos[count]
+    # asignar nombres al alumno
+    alumno["nombres"] = input(f'Ingresar los nombres de alumno {count + 1}:\n')
+
+    # asignar 5 notas al alumno
+    alumno["notas"] = []
+    i = 0
+    while i < 5:
+        try:
+            alumno["notas"].append(int(input(f'Ingrese nota {i + 1}: ')))
+            i += 1
+        except:
+            print("Ingrese sólo números")
+
+    # calcular promedio de 5 notas
+    promedio = 0
+    for nota in alumno["notas"]:
+        promedio += nota
+    alumno["promedio"] = promedio / 5
+
+    # calcular nota mínima y máxima
+    alumno["nota_min"] = min(alumno["notas"])
+    alumno["nota_max"] = max(alumno["notas"])
+
+
+print(alumnos)
